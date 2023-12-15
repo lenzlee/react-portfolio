818a473e5d07261d4b356f62e792d0a67336def5
Lenzlee Wheeler <lwheeler@luc.edu>
2023-12-14 22:38:20 -0600

    updated css and projectitem component

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react"
 
 function ProjectItem({data}) {
-    const { name, img, description } = data
+    const { name, img, url, description } = data
     console.log('hello',data)
     
     return(
         <>
 
          <td>
-            <img class="projectItem" src={img} />
+            <a href={url} title="Launch App"><img title="Click to Launch App" alt="Launch App" class="projectItem" src={img} /></a>
             <h3>{name}</h3>
-            <div class="description"><p>{description}</p></div>
+            <div class="description">
+                <p>{description}</p>
+                <button class="cta" value="Launch App" title="LaunchApp"><a title="Launch App" href={url} target="_blank">Launch the {name} App</a></button>
+            </div>
             </td>
         
         </> 
